8948a077398af97f55578208a09a96a53fea53cc
Eduardo García Díaz <eduardo.gd17@gmail.com>
2024-03-11 15:46:20 -0600

    Hilo actual y propiedades (nombre, prioridad y estado)

--- Program.cs
+++ Program.cs
@@ -7,8 +7,29 @@ class Program
 {
     static void Main()
     {
+        // Obtenemos el hilo actual e imprimimos algunas propiedades
+        Thread currentThread = Thread.CurrentThread;
+        currentThread.Name = "Hilo principal";
+        //Establecemos su prioridad
+        currentThread.Priority = ThreadPriority.Lowest;
+        //Establecemos si corre en segundo plano o no
+        currentThread.IsBackground = false;
+
+        //Imprimirmos sus propiedades
+        Console.WriteLine("Thread Id: {0}", currentThread.ManagedThreadId);
+        Console.WriteLine("Thread Name: {0}", currentThread.Name);
+        Console.WriteLine("Thread State: {0}", currentThread.ThreadState);
+        Console.WriteLine("Es un thread backgorund: {0}", currentThread.IsBackground);
+        Console.WriteLine("Priority: {0}", currentThread.Priority);
+        Console.WriteLine("Culture: {0}", currentThread.CurrentCulture.Name);
+        Console.WriteLine("UI Culture: {0}", currentThread.CurrentUICulture.Name);
+        Console.WriteLine();
+        //////////////////////
+
         //Cree un hilo secundario pasando un delegado ThreadStart
         Thread workerThread = new Thread(new ThreadStart(Print));
+        //Le colocamos nombre al hilo secundario
+        workerThread.Name = "Hilo de Print";
 
         //Iniciar hilo secundario
         workerThread.Start();
@@ -25,6 +46,23 @@ class Program
         //Este codigo es ejecutado por un hilo secundario
         static void Print() 
         {
+            //Obtenemos el hilo actual e imprimimos algunas propiedades
+            Thread currentThread = Thread.CurrentThread;
+            // Establecemos su prioridad
+            currentThread.Priority = ThreadPriority.Highest;
+            //Establecemos si corre en segundo plano o no
+            currentThread.IsBackground = false;
+
+            //Imprimimos sus propiedades
+            Console.WriteLine("Thread Id: {0}", currentThread.ManagedThreadId);
+            Console.WriteLine("Thread Name: {0}", currentThread.Name);
+            Console.WriteLine("Thread State: {0}", currentThread.ThreadState);
+            Console.WriteLine("Es un thread backgorund: {0}", currentThread.IsBackground);
+            Console.WriteLine("Priority: {0}", currentThread.Priority);
+            Console.WriteLine("Culture: {0}", currentThread.CurrentCulture.Name);
+            Console.WriteLine("UI Culture: {0}", currentThread.CurrentUICulture.Name);
+            Console.WriteLine();
+
             for (int i = 11 ; i < 20; i++) 
             {
                 Console.WriteLine($"Print thread: {i} ");
